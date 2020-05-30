75ebcba076e4938ebb1d031ca993a834b170c4b7
CTKnoll <ctk140030@utdallas.edu>
2020-05-29 17:29:11 -0700

    Add core timer and singleton logic

---
+++ Assets/Timer/CoreTimer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System;
+using UnityEngine;
+
+public class CoreTimer : Singleton<CoreTimer>
+{
+    [SerializeField] private float PulseTime;
+    private int CurrentPulse;
+
+    private bool Running;
+    public void TurnTimerOn() => Running = true;
+    public void TurnTimerOff() => Running = false;
+
+    public event Action OnPulse;
+
+    void Start()
+    {
+        Running = true;
+    }
+
+    IEnumerator Timer()
+    {
+        while(Running)
+        {
+            yield return new WaitForSeconds(PulseTime);
+            OnPulse.Invoke();
+            CurrentPulse++;
+        }
+    }
+}
---
+++ Assets/Util/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singleton<T> : MonoBehaviour where T : Singleton<T>
+{
+    private static readonly object _lock = new object();
+    private static T _instance;
+    public static T Instance
+    {
+        get
+        {
+            lock (_lock)
+            {
+                if (_instance == null)
+                {
+                    _instance = (T)FindObjectOfType(typeof(T));
+                    if(_instance == null)
+                    {
+                        Debug.LogWarning("You're trying to access a singleton that's not set in editor!");
+                    }
+                }
+            }
+            return _instance;
+        }
+        private set
+        {
+            _instance = value;
+        }
+    }
+
+    public virtual void OnFirstQueried() { }
+}
